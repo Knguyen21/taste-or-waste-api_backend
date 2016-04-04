85db23a87273a1a30f67fe7ef4ee28f3a1f237f6
khanhnhat <nguyen.k21@gmail.com>
2016-04-04 19:11:23 -0400

    Add category attribute to food model

--- db/migrate/20160402233735_create_foods.rb
+++ db/migrate/20160402233735_create_foods.rb
@@ -2,11 +2,14 @@ class CreateFoods < ActiveRecord::Migration
   def change
     create_table :foods do |t|
       t.string :type
+      t.string :storage
+      t.string :disposal
       t.string :name
       t.text :description
       t.date :purchased_date
       t.date :expiration_date
       t.date :remind_date
+
 
       t.timestamps null: false
     end
