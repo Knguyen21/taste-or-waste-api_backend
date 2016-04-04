77d037bf1147737e121473d3820a54ef012429ce
khanhnhat <nguyen.k21@gmail.com>
2016-04-04 19:06:00 -0400

    Update relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 #
 class User < ActiveRecord::Base
   include Authentication
-  has_many :storages, foreign_key: 'user_id'
   has_many :examples
-  has_many :foods, through: :storages
-  has_many :disposals, foreign_key: 'user_id'
-  has_many :foods, through: :disposals
+  has_many :foods, inverse_of: :user, foreign_key: 'user_id'
 end
