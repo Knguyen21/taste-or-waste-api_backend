22e6b6f3e0f0d517f7de4f63d84771339f7cdfb3
khanhnhat <nguyen.k21@gmail.com>
2016-04-04 19:07:54 -0400

    Update project name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :disposals, except: [:new, :edit]
   resources :foods, except: [:new, :edit]
-  resources :storages, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
