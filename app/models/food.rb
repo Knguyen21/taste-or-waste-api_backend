40444d4057ec77350095bf8c5c46a5450d3a91e3
khanhnhat <nguyen.k21@gmail.com>
2016-04-04 19:05:34 -0400

    add attribute to food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,3 @@
 class Food < ActiveRecord::Base
-    belongs_to :storage, inverse_of: :foods
-    belongs_to :disposal, inverse_of: :foods
-    has_many :users, through: :storages
-    has_many :users, through: :disposals
+    belongs_to :user, inverse_of: :foods
 end
