eafa46cb4b2aba8eef3ba58e6c6fe526b71697bd
khanhnhat <nguyen.k21@gmail.com>
2016-04-05 11:17:08 -0400

    update curl script

--- scripts/food/create-food.sh
+++ scripts/food/create-food.sh
@@ -1,13 +1,13 @@
 curl --include --request POST http://localhost:3000/foods \
-  --header "Authorization: Token token=BAhJIiU5ZGQxNDEwZDcwYThmYTgwMTUxNTg5NjQ1MWYzMjg4ZQY6BkVG--db08b1ee30405b1e3df903ce2a76319071bb2d09" \
+  --header "Authorization: Token token=BAhJIiU2ZjdhZDEyZTRmMGQ0MWY2ZmJjMmNjYzM5YmY5MzQ0ZQY6BkVG--2c0b39b90f44ba1da2b6b54b0c2537afd81dd347" \
   --header "Content-Type: application/json" \
   --data '{
     "food" : {
-    "storage" : "fridge",
-    "disposal" : "eat",
-    "category" : "produce",
-    "name" : "tomato",
-    "description" : "red, plum, and awesome",
+    "storage" : "freezer",
+    "disposal" : "donate",
+    "category" : "dessert",
+    "name" : "ice cream",
+    "description" : "cookies and cream",
     "purchased_date" : "2016-03-21",
     "expiration_date" : "2016-04-01",
     "remind_date" : "2016-03-29"
